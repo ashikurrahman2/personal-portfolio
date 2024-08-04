f8e31b23a03ea20ce94fc9aebd36c29829e87695
Ashikur Rahman <54503492+ashikurrahman2@users.noreply.github.com>
2024-08-04 18:36:49 +0600

    Final Commit
    Final Commit

--- app/Http/Controllers/frontendController.php
+++ app/Http/Controllers/frontendController.php
@@ -15,31 +15,44 @@ class frontendController extends Controller
 {
 
     // Call function all model and file for frontend
-    public function index(){
-        
-        $links= link::all();
+    public function index()
+    {
+
+        $links = link::all();
         $mycv = allcv::all();
-        $contacts= contact::all()->first();
+        $contacts = contact::all()->first();
         $skills = skills::all();
         $services = Service::all();
         $ressumes = ressume::all();
         return view('frontend.pages.Home.home', compact('services', 'ressumes', 'skills', 'contacts', 'mycv', 'links'));
     }
-    // For download CV from frontend
-        public function downloadCV($id)
-        {
-            $cv = allcv::findOrFail($id);
-    
-            // Get the file contents from the database
-            $fileContents = Storage::get($cv->doccument);
-    
-            // Set appropriate headers
-            $headers = [
-                'Content-Type' => 'application/pdf',  // Adjust content type based on file type
-                'Content-Disposition' => 'attachment; filename="' . $cv->doccument . '"',
-            ];
-    
-            // Return the file data as a response
-            return response($fileContents, 200, $headers);
+    // public function downloadCV($id)
+    // {
+    //     $cv = allcv::findOrFail($id);
+
+    //     // Get the file contents from the database
+    //     $fileContents = Storage::get($cv->doccument);
+
+    //     // Set appropriate headers
+    //     $headers = [
+    //         'Content-Type' => 'application/pdf',  // Adjust content type based on file type
+    //         'Content-Disposition' => 'attachment; filename="' . $cv->doccument . '"',
+    //     ];
+
+    //     // Return the file data as a response
+    //     return response($fileContents, 200, $headers);
+    // }
+
+
+    public function downloadCV($id)
+    {
+        $cv = Allcv::findOrFail($id);
+        $filePath = $cv->doccument;
+
+        if (Storage::exists($filePath)) {
+            return Storage::download($filePath);
+        } else {
+            return back()->withErrors(['error' => 'ফাইল পাওয়া যায়নি।']);
         }
+    }
 }
